7548dc8edd995665901da2f2432573e09c853655
John Doe <ZacharySamuel.Fowler@gmail.com>
2023-08-31 19:51:12 -0700

    backend complete, next step is creating front-end react vite app

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import { Book } from "./models/bookModel.js";
+import booksRoute from './routes/booksRoute.js';
 
 const app = express()
 
@@ -9,36 +10,24 @@ const app = express()
 // this is important for testing in postman in the body raw JSON format
 app.use(express.json());
 
+// Middleware for handling CORS POLICY
+// Option 1:Allow All Origins with default of cors(*)
+// app.use(cors());
+// Option 2: Allow Custom Origins
+app.use(
+    cors({
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type'],
+    })
+);
+
 app.get('/', (request, response) => {
     console.log(request)
     return response.status(234).send('Welcome to MERN STACK BOOKSTORE')
 });
 
-// Route for save a new book
-app.post('/books', async (request, response) => {
-    try {
-        if (
-            !request.body.title ||
-            !request.body.author ||
-            !request.body.publishYear
-        ) {
-            return response.status(400).send({
-                message: 'Send all required fields: title, author, publishYear',
-            });
-        }
-        const newBook = {
-            title: request.body.title,
-            author: request.body.author,
-            publishYear: request.body.publishYear,
-        };
-        const book = await Book.create(newBook);
-
-        return response.status(201).send(book);
-    } catch (error) {
-        console.log(error.message);
-        response.status(500).send({message: error.message});
-    }
-});
+app.use('/books', booksRoute);
 
 mongoose
     .connect(mongoDBURL)
