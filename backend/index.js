aa477ea0ff39f960f7d7ccd878f88153e211b4f2
John Doe <ZacharySamuel.Fowler@gmail.com>
2023-09-04 19:12:04 -0700

    created home page, next step is completing show book details

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,8 @@
 import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
-import { Book } from "./models/bookModel.js";
 import booksRoute from './routes/booksRoute.js';
+import cors from "cors";
 
 const app = express()
 
@@ -12,15 +12,15 @@ app.use(express.json());
 
 // Middleware for handling CORS POLICY
 // Option 1:Allow All Origins with default of cors(*)
-// app.use(cors());
+app.use(cors());
 // Option 2: Allow Custom Origins
-app.use(
-    cors({
-        origin: 'http://localhost:3000',
-        methods: ['GET', 'POST', 'PUT', 'DELETE'],
-        allowedHeaders: ['Content-Type'],
-    })
-);
+// app.use(
+//     cors({
+//         origin: 'http://localhost:3000',
+//         methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//         allowedHeaders: ['Content-Type'],
+//     })
+// );
 
 app.get('/', (request, response) => {
     console.log(request)
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,17 +7,16 @@ import ShowBook from './pages/ShowBook'
 import EditBook from './pages/EditBook'
 
 const App = () => {
-
-
+  
   return (
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/books/delete/:id' element={<DeleteBook />} />
-      <Route path='/books/show/:id' element={<ShowBook />} />
+      <Route path='/books/details/:id' element={<ShowBook />} />
       <Route path='/books/create' element={<CreateBook />} />
       <Route path='/books/edit/:id' element={<EditBook />} />
     </Routes>
-  )
-}
+  );
+};
 
 export default App
