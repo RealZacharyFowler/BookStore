4b5fc75272a24a27635b80f991cf46c0a05dfc46
John Doe <ZacharySamuel.Fowler@gmail.com>
2023-09-08 20:36:26 -0700

    completed edit book function

--- frontend/src/pages/CreateBook.jsx
+++ frontend/src/pages/CreateBook.jsx
@@ -11,7 +11,7 @@ const CreateBook = () => {
   const [publishYear, setPublishYear] = useState('');
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
-  const { enqueueSnackbar } = useSnackbar();
+//   const { enqueueSnackbar } = useSnackbar();
 
   const handleSaveBook = () => {
     const data = {
@@ -24,13 +24,13 @@ const CreateBook = () => {
       .post('http://localhost:5555/books', data)
       .then(() => {
         setLoading(false);
-        enqueueSnackbar('Book Created successfully', { variant: 'success' });
+        // enqueueSnackbar('Book Created successfully', { variant: 'success' });
         navigate('/');
       })
       .catch((error) => {
         setLoading(false);
         // alert('An error happened. Please Chack console');
-        enqueueSnackbar('Error', { variant: 'error' });
+        // enqueueSnackbar('Error', { variant: 'error' });
         console.log(error);
       });
   };
