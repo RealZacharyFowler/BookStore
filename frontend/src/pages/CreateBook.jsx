56472562f2f8e7dd3858f55d5ac5e13cc90c28ab
John Doe <ZacharySamuel.Fowler@gmail.com>
2023-09-18 18:05:19 -0700

    finished project

--- frontend/src/pages/CreateBook.jsx
+++ frontend/src/pages/CreateBook.jsx
@@ -3,7 +3,7 @@ import BackButton from '../components/BackButton';
 import Spinner from '../components/Spinner';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-// import { useSnackbar } from 'notistack';
+import { useSnackbar } from 'notistack';
 
 const CreateBook = () => {
   const [title, setTitle] = useState('');
@@ -11,6 +11,7 @@ const CreateBook = () => {
   const [publishYear, setPublishYear] = useState('');
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
+  const {enqueueSnackbar} = useSnackbar();
 
 
   const handleSaveBook = () => {
@@ -24,11 +25,12 @@ const CreateBook = () => {
       .post('http://localhost:5555/books', data)
       .then(() => {
         setLoading(false);
+        enqueueSnackbar('Book Created Successfully', {variant: 'success' });
         navigate('/');
       })
       .catch((error) => {
         setLoading(false);
-        alert('An error happened. Please Chack console');
+        enqueueSnackbar('error', {variant: 'error' });
         console.log(error);
       });
   };
